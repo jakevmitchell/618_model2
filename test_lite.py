89e2aa9d54f9cc7a5af95c22d47c0f62fbf5cdb4
jakevmitchell <62307603+jakevmitchell@users.noreply.github.com>
2022-05-15 13:31:21 -0400

    Add files via upload

--- test_lite.py
+++ test_lite.py
@@ -6,37 +6,41 @@ from tensorflow.python.keras.activations import relu
 from traceloader import TraceConfig
 import numpy
 import logging
-import pynvml
+#import pynvml
 import os
 
-os.chdir("/home/pi/618_model2")
-pynvml.nvmlInit()
-handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+#pynvml.nvmlInit()
+#handle = pynvml.nvmlDeviceGetHandleByIndex(1)
 # Specify the raw data set to be analyzed (e.g.)
-traceset = 'FPGA_PRESENT_RANDOMIZED_CLOCK'
-traceconfig = TraceConfig()
-tracelength = traceconfig.getnrpoints(traceset)
-peakdist = traceconfig.getpeakdistance(traceset)
+#traceset = 'FPGA_PRESENT_RANDOMIZED_CLOCK'
+#traceconfig = TraceConfig()
+#tracelength = traceconfig.getnrpoints(traceset)
+#peakdist = traceconfig.getpeakdistance(traceset)
 
 
 # Define the training and validation parameters
-nrtrain = 5000
-nrval = 10000
-nrepochs = 50
-batchsize = 2000
-filter = 12
-kernel_mult = 2
-strides = 2
-pool = 2
-if nrtrain > 100000:
-    nrsensi = 100000
-else:
-    nrsensi = nrtrain
-balance = 1
+#nrtrain = 0
+#nrval = 500
+#nrepochs = 50
+#batchsize = 2000
+#filter = 12
+#kernel_mult = 2
+#strides = 2
+#pool = 2
+#if nrtrain > 100000:
+#    nrsensi = 100000
+#else:
+#    nrsensi = nrtrain
+#balance = 1
 
-train_x, train_y, val_x, val_y = traceconfig.prep_traces(traceset, nrtrain, nrval, balance)
+print(os.getcwd())
+#train_x, train_y, val_x, val_y = traceconfig.prep_traces(traceset, nrtrain, nrval, balance)
+val_x = numpy.load("val_x.npy")
 
-model = tf.keras.models.load_model("CNNModel")
+val_y = numpy.load("val_y.npy")
+
+model = tf.keras.models.load_model('CNNModel.h5')
+#model = tf.keras.models.load_model("CNNModel")
 model.summary()
 model.compile(optimizer="Adam",loss="mse")
 print(model.evaluate(val_x,val_y))
